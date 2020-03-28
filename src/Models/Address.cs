c7ea44bf0aba1155d7308f2ee14bc4f3dd8a8b42
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 18:12:46 -0300

    Adicionar Models

---
+++ src/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+
+        public int? Number { get; set; }
+
+        public string CityName { get; set; }
+
+        public string StateName { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
---
+++ src/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTimeOffset? DateOfBirth { get; set; }
+
+        public Address HomeAddress { get; set; } = null;
+
+        public bool HasHomeAddress => !(HomeAddress is null);
+
+        public Address BusinessAddress { get; set; } = null;
+
+        public bool HasBusinessAddress => !(BusinessAddress is null);
+    }
+}
