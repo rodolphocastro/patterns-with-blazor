28aac4289c89647bf604d6bcd4424c8ebe9826ad
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 23:55:07 -0300

    Permitir criação através do Funcional

--- src/Models/Builders/Builders.cs
+++ src/Models/Builders/Builders.cs
@@ -1,10 +1,16 @@
-﻿namespace Models.Builders
+﻿using System.Collections.Generic;
+
+namespace Models.Builders
 {
     /// <summary>
     /// Shortcut para acessar os builders.
     /// </summary>
     public class Builders
     {
+        public const string FluentType = "Fluent";
+        public const string FunctionalType = "Functional";
+        public static IEnumerable<string> Types => new List<string> { FluentType, FunctionalType };
+
         public class PersonBuilders
         {
             public static FluentPersonBuilder Fluent => new FluentPersonBuilder();
