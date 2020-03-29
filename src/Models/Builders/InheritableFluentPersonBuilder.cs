9323e3a0500e55045ea9fb0d2bc68c5dde5ce880
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-29 15:48:31 -0300

    Implementar Builder Fluent e Inheritable para pessoas

---
+++ src/Models/Builders/InheritableFluentPersonBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Models.Builders
+{
+    public class InheritableFluentPersonBuilder<T> : PersonBuilder
+        where T : InheritableFluentPersonBuilder<T>
+    {
+        public T Named(string name)
+        {
+            _person.Name = name;
+            return (T)this;
+        }
+
+        public T BornIn(DateTimeOffset? dob)
+        {
+            _person.DateOfBirth = dob;
+            return (T)this;
+        }
+
+        public T LivesAt(Address address)
+        {
+            _person.HomeAddress = address;
+            return (T)this;
+        }
+
+        public T WorksAt(Address address)
+        {
+            _person.BusinessAddress = address;
+            return (T)this;
+        }
+    }
+}
