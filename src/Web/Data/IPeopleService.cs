03720bc3f28d7c1aaebd78a80256db6708a233f5
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 21:36:43 -0300

    Adicionar service para pessoas

---
+++ src/Web/Data/IPeopleService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using Models;
+
+namespace Web.Data
+{
+    public interface IPeopleService
+    {
+        public IReadOnlyCollection<Person> People { get; }
+
+        public int AmountOfPeople { get; }
+
+        public void AddPerson(Person newPerson);
+
+        public void RemovePerson(Person person);
+    }
+}
---
+++ src/Web/Data/PeopleService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Models;
+
+namespace Web.Data
+{
+    public class PeopleService : IPeopleService
+    {
+        private readonly ICollection<Person> _people = new HashSet<Person>();
+        public IReadOnlyCollection<Person> People => _people.ToList();
+
+        public int AmountOfPeople => People.Count();
+
+        public void AddPerson(Person newPerson)
+        {
+            newPerson.Id = People.Count + 1;
+            _people.Add(newPerson);
+        }
+
+        public void RemovePerson(Person person) => _people.Remove(person);
+    }
+}
