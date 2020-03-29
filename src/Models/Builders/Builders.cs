821722f408996004b9e6384f57ffaf838ecb82ae
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-29 15:48:47 -0300

    Permitir utilização do InheritableBuilder

--- src/Models/Builders/Builders.cs
+++ src/Models/Builders/Builders.cs
@@ -10,13 +10,20 @@ namespace Models.Builders
         public const string FluentType = "Fluent";
         public const string FunctionalType = "Functional";
         public const string FacetedType = "Faceted";
-        public static IEnumerable<string> Types => new List<string> { FluentType, FunctionalType, FacetedType };
+        public const string Inheritable = "Inheritable Fluent";
+        public static IEnumerable<string> Types => new List<string> { FluentType, FunctionalType, FacetedType, Inheritable };
 
         public class PersonBuilders
         {
             public static FluentPersonBuilder Fluent => new FluentPersonBuilder();
             public static FunctionalPersonBuilder Functional => new FunctionalPersonBuilder();
             public static FacetedPersonBuilder Faceted => new FacetedPersonBuilder();
+            public static InheritableBuilder InheritableFluent => new InheritableBuilder();
+
+            public class InheritableBuilder : InheritableFluentPersonBuilder<InheritableBuilder>
+            {
+                internal InheritableBuilder() { }
+            }
         }
 
         public class AddressBuilders
