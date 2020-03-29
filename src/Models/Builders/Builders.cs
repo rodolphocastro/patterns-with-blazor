a509348b24224f29034d4777e25f677471e55e5c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 23:35:57 -0300

    Adicionar Builder funcional para Address

--- src/Models/Builders/Builders.cs
+++ src/Models/Builders/Builders.cs
@@ -8,11 +8,13 @@
         public class PersonBuilders
         {
             public static FluentPersonBuilder Fluent => new FluentPersonBuilder();
+            public static FunctionalPersonBuilder Functional => new FunctionalPersonBuilder();
         }
 
         public class AddressBuilders
         {
             public static FluentAddressBuilder Fluent => new FluentAddressBuilder();
+            public static FunctionalAddressBuilder Functional => new FunctionalAddressBuilder();
         }
     }
 }
---
+++ src/Models/Builders/FunctionalAddressBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.Builders
+{
+    public class FunctionalAddressBuilder : AddressBuilder
+    {
+        protected readonly List<Action<Address>> _actions = new List<Action<Address>>();
+
+        public IList<Action<Address>> Actions => _actions;
+
+        public new Address Build()
+        {
+            _actions.ForEach(a => a(_address));
+            return base.Build();
+        }
+    }
+
+    public static class FunctionalAddressBuilderExtensions
+    {
+        public static FunctionalAddressBuilder AtStreet(this FunctionalAddressBuilder builder, string street)
+        {
+            builder.Actions.Add(a => a.Street = street);
+            return builder;
+        }
+
+        public static FunctionalAddressBuilder AtNumber(this FunctionalAddressBuilder builder, int? number)
+        {
+            builder.Actions.Add(a => a.Number = number);
+            return builder;
+        }
+
+        public static FunctionalAddressBuilder AtCity(this FunctionalAddressBuilder builder, string city)
+        {
+            builder.Actions.Add(a => a.CityName = city);
+            return builder;
+        }
+
+        public static FunctionalAddressBuilder AtState(this FunctionalAddressBuilder builder, string state)
+        {
+            builder.Actions.Add(a => a.StateName = state);
+            return builder;
+        }
+
+        public static FunctionalAddressBuilder AtCountry(this FunctionalAddressBuilder builder, string country)
+        {
+            builder.Actions.Add(a => a.CountryName = country);
+            return builder;
+        }
+    }
+}
