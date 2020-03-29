37903b33813be74b8b560cfc42f3003ec879657c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 22:49:41 -0300

    Adicionar Builder para Address

---
+++ src/Models/Builders/AddressBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Models.Builders
+{
+    /// <summary>
+    /// Base para os builders de Address.
+    /// </summary>
+    public abstract class AddressBuilder
+    {
+        protected Address _address = new Address();
+
+        public Address Build() => _address;
+    }
+}
---
+++ src/Models/Builders/FluentAddressBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace Models.Builders
+{
+    public class FluentAddressBuilder : AddressBuilder
+    {
+        public FluentAddressBuilder WithCountry(string country)
+        {
+            _address.CountryName = country;
+            return this;
+        }
+
+        public FluentAddressBuilder WithState(string state)
+        {
+            _address.StateName = state;
+            return this;
+        }
+
+        public FluentAddressBuilder WithCity(string city)
+        {
+            _address.CityName = city;
+            return this;
+        }
+
+        public FluentAddressBuilder WithNumber(int? number)
+        {
+            _address.Number = number;
+            return this;
+        }
+
+        public FluentAddressBuilder WithStreet(string street)
+        {
+            _address.Street = street;
+            return this;
+        }
+    }
+}
