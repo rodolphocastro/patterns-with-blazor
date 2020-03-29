0f55ebd98b88dcb298235675013c4a5e815cdbaf
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-29 00:45:28 -0300

    Adicionar o faceted builder

--- src/Models/Builders/Builders.cs
+++ src/Models/Builders/Builders.cs
@@ -9,12 +9,14 @@ namespace Models.Builders
     {
         public const string FluentType = "Fluent";
         public const string FunctionalType = "Functional";
-        public static IEnumerable<string> Types => new List<string> { FluentType, FunctionalType };
+        public const string FacetedType = "Faceted";
+        public static IEnumerable<string> Types => new List<string> { FluentType, FunctionalType, FacetedType };
 
         public class PersonBuilders
         {
             public static FluentPersonBuilder Fluent => new FluentPersonBuilder();
             public static FunctionalPersonBuilder Functional => new FunctionalPersonBuilder();
+            public static FacetedPersonBuilder Faceted => new FacetedPersonBuilder();
         }
 
         public class AddressBuilders
