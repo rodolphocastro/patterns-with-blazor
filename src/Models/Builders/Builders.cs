79b782a5fcf64768a1926bef3f91d79f79683018
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-28 22:34:51 -0300

    Adicionar o FluentBuilder

---
+++ src/Models/Builders/Builders.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Builders
+{
+    /// <summary>
+    /// Shortcut para acessar os builders.
+    /// </summary>
+    public class Builders
+    {
+        public static FluentPersonBuilder Fluent => new FluentPersonBuilder();
+    }
+}
---
+++ src/Models/Builders/PersonBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Models.Builders
+{
+    /// <summary>
+    /// Base para os builders de Pessoas.
+    /// </summary>
+    public abstract class PersonBuilder
+    {
+        protected Person _person = new Person();
+
+        public Person Build() => _person;
+    }
+}
