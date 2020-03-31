b579bee24ae71de3b004840b4a39e8e4d50d9a13
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-30 23:19:07 -0300

    Adicionar model Vehicle

---
+++ src/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public int? Wheels { get; set; }
+
+        public int Doors { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }        
+    }
+}
