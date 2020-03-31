0507aff348231452268e2f1dae570cb0f51a6c4d
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-30 23:27:04 -0300

    Adicionar fabricantes

---
+++ src/Models/VehicleMakes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public static class VehicleMakes
+    {
+        private const string _renault = "Renault";
+        private const string _chevy = "Chevrolet";
+        private const string _wolks = "Volkswagen";
+        private const string _ford = "Ford";
+        private const string _tesla = "Tesla";
+        private const string _honda = "Honda";
+        private const string _harley = "Harley Davidson";
+        private const string _kawasaki = "Kawasaki Motors";
+
+        public static IEnumerable<string> Makes => new List<string>
+        {
+            _renault,
+            _chevy,
+            _wolks,
+            _ford,
+            _tesla,
+            _honda,
+            _harley,
+            _kawasaki
+        };
+    }
+}
