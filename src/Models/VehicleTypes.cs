1c2a0b269c321ed68015ec7a9fb7b420f04fc75b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-30 23:36:20 -0300

    Adicionar tipos de Veículos

---
+++ src/Models/VehicleTypes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class VehicleTypes
+    {
+        public const string Car = "Car";
+        public const string Motorcycle = "Motorcycle";
+        public const string Spaceship = "Spaceship";
+        public const string Unicycle = "Unicycle";
+        public static IEnumerable<string> TypesList = new List<string>
+        {
+            Car,
+            Motorcycle,
+            Spaceship,
+            Unicycle
+        };
+    }
+}
