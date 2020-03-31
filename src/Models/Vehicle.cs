2771b028383c56965fdc87ed5da42e348dcf95e3
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-30 23:35:56 -0300

    Adicionar MethodFactory

--- src/Models/Vehicle.cs
+++ src/Models/Vehicle.cs
@@ -2,14 +2,21 @@
 {
     public class Vehicle
     {
+        internal Vehicle() { }
+
         public int Id { get; set; }
 
-        public int? Wheels { get; set; }
+        public int? Wheels { get; set; } = null;
 
-        public int Doors { get; set; }
+        public int? Doors { get; set; } = null;
 
         public string Make { get; set; }
 
-        public string Model { get; set; }        
+        public string Model { get; set; }
+
+        public static Vehicle CreateNewMotorcycle(int id, string make, string model) => new Vehicle { Id = id, Wheels = 2, Make = make, Model = model };
+        public static Vehicle CreateNewUnicycle(int id, string make, string model) => new Vehicle { Id = id, Wheels = 1, Make = make, Model = model };
+        public static Vehicle CreateNewSpaceship(int id, string make, string model) => new Vehicle { Id = id, Make = make, Model = model };
+        public static Vehicle CreateNewCar(int id, string make, string model, int doors = 2) => new Vehicle { Id = id, Wheels = 4, Doors = doors, Make = make, Model = model };
     }
 }
