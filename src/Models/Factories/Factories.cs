9894996f4987c0a3182778785be98421393b7498
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-05 18:58:55 -0300

    Corrigir typo na lista de Factories

--- src/Models/Factories/Factories.cs
+++ src/Models/Factories/Factories.cs
@@ -10,6 +10,6 @@ namespace Models.Factories
         public const string MethodFactory = "Method";
         public const string ClassFactory = "Class";
         public const string AbstractFactory = "Abstract";
-        public static IEnumerable<string> Types = new List<string> { MethodFactory, ClassFactory, MethodFactory };
+        public static IEnumerable<string> Types = new List<string> { MethodFactory, ClassFactory, AbstractFactory };
     }
 }
