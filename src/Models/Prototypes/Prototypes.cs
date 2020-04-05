b0de26ecb5cdeffa5b4804843ebe3dee14284558
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-05 20:33:15 -0300

    Adicionar estratégias de Prototype

---
+++ src/Models/Prototypes/Prototypes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Models.Prototypes
+{
+    public class Prototypes
+    {
+        public const string None = "None";
+        public const string Instances = "Static Instances";
+        public const string Cloneable = "ICloneable";
+        public const string DeepCopyable = "DeepCopyable";
+        public const string ShallowCopyable = "ShallowCopyable";
+        public static IEnumerable<string> PrototypeTypes = new List<string>
+        {
+            None,
+            Instances,
+            Cloneable,
+            DeepCopyable,
+            ShallowCopyable
+        };
+    }
+}
