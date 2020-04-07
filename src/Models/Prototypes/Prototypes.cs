070aec27aef53c76cf3e13a30401bf5cacd18d73
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-06 21:09:11 -0300

    Adicionar outros tipo de prototype

--- src/Models/Prototypes/Prototypes.cs
+++ src/Models/Prototypes/Prototypes.cs
@@ -6,16 +6,20 @@ namespace Models.Prototypes
     {
         public const string None = "None";
         public const string Instances = "Static Instances";
+        public const string CopyCtor = "Copy Constructor";
         public const string Cloneable = "ICloneable";
         public const string DeepCopyable = "DeepCopyable";
         public const string ShallowCopyable = "ShallowCopyable";
+        public const string Serializable = "Serializable";
         public static IEnumerable<string> PrototypeTypes = new List<string>
         {
             None,
             Instances,
+            CopyCtor,
             Cloneable,
             DeepCopyable,
-            ShallowCopyable
+            ShallowCopyable,
+            Serializable
         };
     }
 }
